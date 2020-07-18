66177e0b601a41436c85ee3847720901771669fd
battletremor <battletremor@gmail.com>
2020-07-18 20:34:43 +0530

    version1

---
+++ android/app/src/main/kotlin/battletremor/co/pac_man/MainActivity.kt
@@ -0,0 +1,6 @@
+package battletremor.co.pac_man
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
+import 'package:box2d_flame/box2d.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:flame/game.dart';
+import 'dart:async';
+
+
+void main() async{
+  Util flameUtil = Util();
+  Util
+
+}
+
+
